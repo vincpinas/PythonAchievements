ddca94daf98a96d60d2246f8ba33bdff783e47f7
vincent <ikbengerrit92@gmail.com>
2020-09-09 15:07:53 +0200

    Print een Poem door 2 verses te defineren.

---
+++ PYTB1L2PoemPrinting/PoemPrinting.py
@@ -0,0 +1,4 @@
+vers1= "Ik ben vincent, ik woon in een huis en daarom blijf ik graag thuis en heb ik een hekel aan krekels."
+vers2= "Ik speel graag warframe en dat is geen leugen want de mensen die het spelen deugen."
+
+print(vers1,vers2)
