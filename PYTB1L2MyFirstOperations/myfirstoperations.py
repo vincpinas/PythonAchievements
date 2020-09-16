669208dccf0190a6776d4b5ee0cce0502a52feb3
vincent <ikbengerrit92@gmail.com>
2020-09-16 15:02:05 +0200

    MyFirstOperations Finish

---
+++ PYTB1L2MyFirstOperations/myfirstoperations.py
@@ -0,0 +1,22 @@
+x = 2
+a = 7
+z = 5
+w = 1
+q = 3
+
+berek1 = x + a
+print("berekening1: x + a= ", berek1)
+berek2 = q - w
+print("berekening2: q - w= ", berek2)
+berek3 = z / x
+print("berekening3: z - x= ", berek3)
+berek4 = q % z
+print("berekening4: q - z= ", berek4)
+berek5 = a
+berek5 += w
+print("berekening5: a += w  =", berek5)
+berek6 = z
+berek6 /= q
+print("berekening6: z /= q  =", berek6)
+berek7 = z * a
+print("berekening7: z * a= ", berek7)
