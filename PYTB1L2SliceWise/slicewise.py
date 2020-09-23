52902d84808c2f4df39b623e7d452d24bc565de7
vincent <ikbengerrit92@gmail.com>
2020-09-23 14:53:27 +0200

    Sliced opdracht af

---
+++ PYTB1L2SliceWise/slicewise.py
@@ -0,0 +1,27 @@
+tekst1 = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+Erik = tekst1[37:42]
+print(Erik)
+Erwin = tekst1[43:48]
+print(Erwin)
+Hidde = tekst1[61:66]
+print(Hidde) 
+
+tekst2 = 'SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ...'
+Python = tekst2[15:21]
+print(Python)
+UXD = tekst2[23:26]
+print(UXD)
+FrontEnd = tekst2[28:48]
+print(FrontEnd)
+BackEnd = tekst2[52:72]
+print(BackEnd)
+
+tekst3 = 'Wat is hier het laatste woord?'
+laatste = tekst3[24:29]
+print(laatste)
+
+tekst4 = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+vijftot8 = tekst4[4:8]
+print(vijftot8)
+negen20tot33 = tekst4[28:33]
+print(negen20tot33)
