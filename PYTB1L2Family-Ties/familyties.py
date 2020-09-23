cf072cee6ad40aeb04846ff641243e3a30f3a3a7
vincent <ikbengerrit92@gmail.com>
2020-09-23 14:15:37 +0200

    Family/eigen values opgeslagen en gebruikt om te printen

---
+++ PYTB1L2Family-Ties/familyties.py
@@ -0,0 +1,11 @@
+naam = "vincent"
+naamPrint = naam[0] .upper() + naam[1:] .lower()
+leeftijd = "17"
+woonplaats = "Uithoorn"
+naarBus = "5 min"
+totaalTrip = "1 uur"
+zusLeeftijd = "18"
+
+
+print("Mijn naam is " + naamPrint + ", " + "ik ben " + leeftijd + " jaar oud " + "en mijn zus is " + zusLeeftijd + " jaar oud " + "we wonen samen in " + woonplaats + ".")
+print("Ik woon ongeveer " + naarBus + " van het busstation af," + " de hele trip naar school duurt ongeveer " + totaalTrip + "." )
