3bb82bd5907185dda6c2bf959542c39f45619e50
vincent <ikbengerrit92@gmail.com>
2020-09-25 15:27:21 +0200

    Deeply Nested programma geschreven

---
+++ PYTB1L3DeeplyNested/DeeplyNested.py
@@ -0,0 +1,14 @@
+honger = True
+geenZin = True
+bestelPizza = print("Bestel een double pepperoni piza.")
+kliekje = False
+money = 1600
+
+
+if honger == True and geenZin == True:
+    if money > 1300:
+        bestelPizza
+        if kliekje == True:
+            print("Eet het kliekje in de koelkast.")
+    else:
+        print("Toch maar even koken.")
