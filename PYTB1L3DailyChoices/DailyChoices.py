e81ae5085c3f8b0581f48104687164bd07e63fc2
vincent <ikbengerrit92@gmail.com>
2020-09-30 14:23:45 +0200

    4 extra opties added

--- PYTB1L3DailyChoices/DailyChoices.py
+++ PYTB1L3DailyChoices/DailyChoices.py
@@ -1,6 +1,7 @@
 print()
 
 while True:
+    #question1
     answer = input("Did you eat breakfast today?")
     while True:
         if answer == "yes" or answer == "YES" or answer == "Y" or answer == "y":
@@ -11,3 +12,65 @@ while True:
             break
         else: 
             ("Invalid Input")
+
+while True:
+    #question2
+    answer = input("Did you eat fruit, yoghurt or minecraft today?")
+    while True:
+        if answer == "fruit" or answer == "FRUIT" or answer == "Fruit":
+            print("FRUITFRUITFRUITFRUITFRUITFRUITFRUIT!!!!!??!?!?!??!?!?")
+            break
+        elif answer == "yoghurt" or answer == "YOGHURT" or answer == "Yoghurt":
+            print("I've heard yoghurt makes you fat...")
+            break
+        elif answer == "minecraft" or answer == "MINECRAFT" or answer == "Minecraft":
+            print("That's not really good for you...")
+            break
+        else: 
+            ("Invalid Input")
+
+
+while True:
+    #question3
+    answer = input("Did you go to school?")
+    while True:
+        if answer == "yes" or answer == "YES" or answer == "Y" or answer == "y":
+            print("You're not getting any praises from me, going to school in this country is forced")
+            break
+        elif answer == "no" or answer == "NO" or answer == "N" or answer == "n":
+            print("What did you do then?")
+            break
+        else: 
+            ("Invalid Input")
+
+
+while True:
+    #question4
+    answer = input("do you use a pc?")
+    while True:
+        if answer == "yes" or answer == "YES" or answer == "Y" or answer == "y":
+            print("you're gonna need that for this course")
+            break
+        elif answer == "no" or answer == "NO" or answer == "N" or answer == "n":
+            print("how are you even running this program?")
+            break
+        else: 
+            ("Invalid Input")
+
+
+while True:
+    #question5
+    answer = input("Do you hate programming?")
+    while True:
+        if answer == "yes" or answer == "YES" or answer == "Y" or answer == "y":
+            print("what are you even doing here?")
+            break
+        elif answer == "no" or answer == "NO" or answer == "N" or answer == "n":
+            print("The correct answer would be a love/hate relationship")
+            break
+        else: 
+            ("Invalid Input")
+
+
+print("-------------------------------------------------------------------------")
+print("End of programm.")
