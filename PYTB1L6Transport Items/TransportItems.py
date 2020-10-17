b0e907568abeac71ba5649b6535e0d502aebed5a
vincent <ikbengerrit92@gmail.com>
2020-10-17 18:39:54 +0200

    transport items finished

---
+++ PYTB1L6Transport Items/TransportItems.py
@@ -0,0 +1,26 @@
+import time, os, sys
+product = input("what product would you like to make? ")
+message = product + " is in development\n"
+for char in message:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.06)
+factory = []
+factory.append(product)
+time.sleep(1)
+os.system("cls")
+print("factory has finished making the product " +str(factory))
+time.sleep(1.7)
+print("factory is sending the " + str(factory) +" to distribution")
+time.sleep(1.7)
+os.system("cls")
+distribution = []
+distribution.append(factory)
+factory.clear
+print("distribution is preparing the " + str(distribution) + " for sale")
+time.sleep(1.7)
+os.system("cls")
+shop = []
+shop.append(distribution)
+distribution.clear
+print("The " + str(shop) + " is ready for sale in the shop")
