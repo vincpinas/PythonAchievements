bebe6c2468e602d42bda0367fd5a9c67d7d0f235
vincent <ikbengerrit92@gmail.com>
2020-10-21 13:54:14 +0200

    v2

---
+++ PYTB1L5RandomShuffle/randomshufflev2.py
@@ -0,0 +1,8 @@
+import random
+def randomised():
+    original = "vincent"
+    randomised = ''.join(random.sample(original, len(original)))
+    print(randomised)
+
+for i in range(3):
+    randomised()
