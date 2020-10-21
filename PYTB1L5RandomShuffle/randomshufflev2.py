24413a23d2081a4c0579a8e2bb96b7b0fd5cee1a
vincent <ikbengerrit92@gmail.com>
2020-10-21 14:35:14 +0200

    version2

--- PYTB1L5RandomShuffle/randomshufflev2.py
+++ PYTB1L5RandomShuffle/randomshufflev2.py
@@ -1,8 +1,14 @@
 import random
-def randomised():
-    original = "vincent"
-    randomised = ''.join(random.sample(original, len(original)))
-    print(randomised) 
+original = ["vincent", "heerlijk", "smakelijk"]
 
-for i in range(3):
-    randomised()
+randomised = ''.join(random.sample(original[0], len(original[0])))
+randomised2 = ''.join(random.sample(original[1], len(original[1])))
+randomised3 = ''.join(random.sample(original[2], len(original[2])))
+
+def shuffle(randomised):
+    print(randomised, randomised2, randomised3)
+    return 3 * randomised 
+
+shuffle(randomised)
+
+
